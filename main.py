dab15a2f341e8698913e85a4779d6355ac14d8c9
DungeonMaster <ralnikovp@gmail.com>
2025-09-16 12:42:58 +0300

    Add README.md

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from dotenv import load_dotenv
 from cf_request import *
+import sys
 
-def main():
-    load_dotenv()
-    r = cf_request('contest.hacks', {
-        "contestId": "566",
-        "asManager": "false"
+def process_group(group_code : str):
+    r = cf_request('contest.list', {
+        "gym": "true",
+        "groupCode": group_code
     })
+
     print(r.status_code)
     print(r.json())
 
+def main(args):
+    load_dotenv()
+    group_codes = args[1:]
+
+    for group_code in group_codes:
+        process_group(group_code)
+
+
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
